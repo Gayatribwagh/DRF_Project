0b3b98955c5dab82f85cbd960581e15d95e62f20
Gayatribwagh <121668400+Gayatribwagh@users.noreply.github.com>
2024-10-21 10:32:53 +0530

    Add files via upload

---
+++ serializers.py
@@ -0,0 +1,8 @@
+# task_app/serializers.py
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'due_date', 'status']
---
+++ views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+
+# Create your views here.
+# task_app/views.py
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Task
+from .serializers import TaskSerializer
+from django.shortcuts import get_object_or_404
+
+class TaskListCreateView(APIView):
+    # Retrieve all tasks (GET) and create new task (POST)
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskDetailView(APIView):
+    # Retrieve, update, and delete a specific task by id (GET, PUT, DELETE)
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
